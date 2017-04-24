5bd712f9a9da43cf6c6c3896501ec1e07f2ddd68
Kalpz95 <kgam2126@uni.sydney.edu.au>
2017-04-24 16:14:50 +1000

    unit tests 2

--- flaskr/flaskr_tests.py
+++ flaskr/flaskr_tests.py
@@ -1,31 +1,3 @@
-"""import os
-import flaskr
-import unittest
-import tempfile
-
-
-class UserLoginTestCase(unittest.TestCase):
-    def setUp(self):
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username = username,
-            password = password
-        ),follow_redirects=True)
-
-    def test_login_ok(self):
-        rv = self.login("test", "test123")
-        assert b'Success' in rv.data
-
-    def test_login_unknown_user(self):
-        rv = self.login("baduser", "badpasswd")
-        assert b'Fail' in rv.data
-
-if __name__ == '__main__':
-    unittest.main()"""
-
 import os
 import tempfile
 import pytest
@@ -60,13 +32,11 @@ def logout(client):
 
 
 def test_empty_db(client):
-    """Start with a blank database."""
     rv = client.get('/')
-    assert b'No entries here so far' in rv.data
+    assert b'Unbelievable.  No entries here so far' in rv.data
 
 
 def test_login_logout(client):
-    """Make sure login and logout works"""
     rv = login(client, flaskr.app.config['USERNAME'],
                flaskr.app.config['PASSWORD'])
     assert b'You were logged in' in rv.data
@@ -78,5 +48,7 @@ def test_login_logout(client):
     rv = login(client, flaskr.app.config['USERNAME'],
                flaskr.app.config['PASSWORD'] + 'x')
     assert b'Invalid password' in rv.data
+
+
 
 
